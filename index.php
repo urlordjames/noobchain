f1e98e328a867aceaf40834359c881489127f6fa
SolSahn <solsahn@gmail.com>
2018-09-16 20:59:14 -0400

    Added monospace CSS into body tag

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 	$messages = fread($mfile, filesize("messages.bc"));
 	fclose($mfile);
 	echo($messages . "<br>");
-	//TODO: add a monospace font with css
 ?>
 <html>
 	<head>
 		<title>noobchain</title>
 		<link rel="shortcut icon" type="image/x-icon" href="https://urlordjames.ga/favicon.ico">
 	</head>
-	<body>
+	<body style="font-family: monospace">
 		<!--send help-->
 	</body>
 </html>
