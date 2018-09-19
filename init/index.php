79f9ed4e129101d07df1077d1b21986c761063ea
urlordjames <urlordjames@gmail.com>
2018-09-19 15:47:25 -0400

    started work on authentication

---
+++ init/index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>authenticate</title>
+		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+	</head>
+	<body>
+	<form action="init.php" method="post">
+		enter your password: <input type="password" name="pass"><br>
+		<div class="g-recaptcha" data-sitekey="6LfkJHEUAAAAAP7lzVSDN5rF-E0z0Zvau3c34-B-"></div>
+		<input type="submit">
+	</form>
+	</body>
+</html>
