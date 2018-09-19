b25a2550591b2be741838168a415ab67efe384fb
urlordjames <urlordjames@gmail.com>
2018-09-19 15:51:59 -0400

    quick fix

--- init/index.php
+++ init/index.php
@@ -5,7 +5,7 @@
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 	</head>
 	<body>
-	<form action="init.php" method="post">
+	<form action="../init.php" method="post">
 		enter your password: <input type="password" name="pass"><br>
 		<div class="g-recaptcha" data-sitekey="6LfkJHEUAAAAAP7lzVSDN5rF-E0z0Zvau3c34-B-"></div>
 		<input type="submit">
